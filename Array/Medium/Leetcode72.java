f9ec06176f7868b92d826e99ce26b4380aa746d1
Himanshujadhav2004 <himanshujadhav341@gmail.com>
2026-04-24 23:57:36 +0530

    medium Leetcode72

---
+++ Array/Medium/Leetcode72.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void sortColors(int[] nums) {
+//brute force
+
+int n = nums.length;
+int c1=0;
+int c2=0;
+int c3=0;
+
+for(int i=0;i<n;i++){
+    if(nums[i]==0){
+        c1++;
+    }
+    else if(nums[i]==1){
+        c2++;
+    }
+    else{
+        c3++;
+    }
+}
+
+for(int i=0;i<c1;i++){
+    nums[i]=0;
+}
+
+for(int i=c1;i<c1+c2;i++){
+    nums[i]=1;
+}
+for(int i=c1+c2;i<c1+c2+c3;i++){
+    nums[i]=2;
+}
+
+
+    }
+}
